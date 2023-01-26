fdff84ef8cb20745ea8cb1d1a075af443b32f8f9
JMHere <jmendez@student.neumont.edu>
2023-01-26 08:22:25 -0700

    Getting the values

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const tripT = document.getElementById("selectType")
+const depart = document.getElementById("depart")
+const destination = document.getElementById("destination")
+const aPassangers = document.getElementById("aPassanger")
+const cClass = document.getElementById("class")
+
+function finalForm() {
+    console.log(tripT.value, depart.value, destination.value, aPassangers.value, cClass.value)
+}
+
