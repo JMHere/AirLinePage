91ba7378d0602ec7512c4904b727a19516245214
JMHere <jmendez@student.neumont.edu>
2023-02-01 09:47:04 -0700

    few changes

--- index.js
+++ index.js
@@ -13,11 +13,13 @@ app.use(express.static(path.join(__dirname, '/public')))
 app.use('/public/images/', express.static('./public/images'));
 
 app.set('views', path.join(__dirname, 'views'))
+
 app.set('view engine', 'ejs')
 
 app.get('/', function(req, res, next){
     res.render('index') 
 })
+
 app.get('/home', function(req, res, next){
     res.render('home')
 })
@@ -27,7 +29,9 @@ app.get('/log', function(req, res, next){
 })
 
 app.post('/ticketP', urlencodedParser, ticket.getData)
+
 app.get('/purchasePage/:id', ticket.PpId)
+
 app.listen(3000)
 
 
