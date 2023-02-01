6ff5971343e98c0b59f2eb32999406f35963f8a2
joflores18 <100979201+joflores18@users.noreply.github.com>
2023-02-01 09:46:34 -0700

    scroll

--- index.js
+++ index.js
@@ -20,7 +20,5 @@ app.get('/home', function(req, res, next){
     res.render('home')
 })
 app.post('/ticketP', urlencodedParser, ticket.getData)
-app.get('/purchasePage', function(req, res ,next){
-    res.render('purchasePage')
-})
+app.get('/purchasePage/:id', urlencodedParser, ticket.PpId)
 app.listen(3000)
