a01bf309f22d04d9d6869e3492397f169cb05344
cwhited <cwhited@student.neumont.edu>
2023-02-01 09:53:48 -0700

    Fixed imgs

--- index.js
+++ index.js
@@ -26,6 +26,15 @@ app.get('/log', function(req, res, next){
     res.render('sign-up')
 })
 
+app.get('/im', function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/imgs/plane2.pdf'));
+});
+
+app.get('/im2', function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/imgs/planeSeat.png'));
+});
+
+
 app.post('/ticketP', urlencodedParser, ticket.getData)
 app.get('/purchasePage/:id', ticket.PpId)
 app.listen(3000)
